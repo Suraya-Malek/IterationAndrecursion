6040f15289e6922146f356a2b2aceb598ccaed8a
username <useremail>
2023-10-15 15:35:16 -0700

    Iterative and recursive functionality work.

--- IterationAndrecursion.py
+++ IterationAndrecursion.py
@@ -1,1 +1,18 @@
+print("iterative version")
+num=int(input("enter a number:"))
+factorial = 1
 
+if num<0:
+    print("no factorial")
+if num >= 0:
+    for t in range(1, 1+num):
+        factorial = t*factorial
+print(factorial)
+
+print("recursive version")
+
+def fact(i):
+    return 1 if (i==1 or i==0) else i*fact(i-1);
+
+print(fact(num))
+    
